a44ae3b59899d7a70d20e05a8071deb56f22c519
Bhimanshu <bhimanshu.durge.bd@gmail.com>
2024-08-14 17:29:38 +0530

    created signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,11 +14,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        unique: true,
-    }
+    },
 }, {timestamps: true}
 );
 
-const User = mongoose.model('User,userSchema');
+const User = mongoose.model('User',userSchema);
 
 export default User;
