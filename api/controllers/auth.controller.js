bf941d560a38d4c04ed3b5514cc6b24ffcea6430
Bhimanshu <bhimanshu.durge.bd@gmail.com>
2024-08-20 00:50:01 +0530

    .

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -80,7 +80,7 @@ export const google = async (req,res,next) => {
                 profilePicture: googlePhotoUrl, 
             });
             await newUser.save();
-            const token = jwt.sign({id: newUser._id, isAdmin.newUser.isAdmin},process.env.JWT_SECRET);
+            const token = jwt.sign({id: newUser._id, isAdmin:newUser.isAdmin},process.env.JWT_SECRET);
             const {password, ...rest} = user._doc;
             res.status(200).cookie('access_token',token, {
                 httpOnly: true,
